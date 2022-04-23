d90d948a4e1afb16a27685c215d86cb513425b3c
Nicko <nickos@iMac.local>
2022-04-23 11:28:05 +0200

    first commit

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+export default function index() {
+	return (
+		<div>
+			<iframe
+				style={{borderRadius:12}}
+				src="https://open.spotify.com/embed/album/64SMUvTpjWyQNWaiXy2m24?utm_source=generator"
+				width="100%"
+				height="380"
+				frameBorder="0"
+				allowfullscreen=""
+				allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+			></iframe>
+		</div>
+	);
+}
