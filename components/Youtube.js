c8682997fcae1bf4bce7ce8bc86b43c92b5fe75d
Nicko <nickos@iMac.local>
2022-04-26 20:51:52 +0200

    add youtube section

--- components/Youtube.js
+++ components/Youtube.js
@@ -1,9 +1,18 @@
 import React from "react";
 
-export default function Youtube() {
-  return (
-    <div>
-      
-    </div>
-  );
+export default function Youtube(props) {
+	return (
+		<div>
+			<iframe
+				className={props.className}
+				width="560"
+				height="315"
+				src={props.src}
+				title={props.title}
+				frameBorder="0"
+				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+				allowFullScreen
+			></iframe>
+		</div>
+	);
 }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Album from "../components/Album";
 import Video from "../components/Video";
+import Youtube from "../components/Youtube";
 import styles from "../styles/Home.module.scss";
 export default function Homepage() {
 	return (
@@ -21,6 +22,13 @@ export default function Homepage() {
 				<Album alt="pochette de l'album l'impasse" title="l'impasse" src="/images/impasse.webp" year="2019">
 					L'impasse
 				</Album>
+			</div>
+			<div className={styles.youtubeContainer}>
+				<Youtube
+					title="Youtube video 'Entretien d'embauche - Untel'"
+					src="https://www.youtube.com/embed/1JbqsjB4qPg"
+				/>
+				<Youtube title="Youtube video 'Favorable - Untel'" src="https://www.youtube.com/embed/fyi2exJt24U" />
 			</div>
 		</div>
 	);
